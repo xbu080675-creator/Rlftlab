d960d012d1d7586f772d676dd49825d455c10654
PocketSpace <xbu080675@gmail.com>
2026-09-09 02:19:36 +0800

    fix: use RowScope weight in matchup component

--- app/src/main/java/com/riftlab/app/ui/TeamMatchupVisual.kt
+++ app/src/main/java/com/riftlab/app/ui/TeamMatchupVisual.kt
@@ -5,7 +5,6 @@ import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.layout.weight
 import androidx.compose.foundation.layout.widthIn
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
