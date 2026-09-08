cfc8f3f575a1d22e365dbe42e31167b4e2580d75
PocketSpace <xbu080675@gmail.com>
2026-09-08 15:14:55 +0800

    ui: apply Rift theme to schedule center root

--- app/src/main/java/com/riftlab/app/MainActivity.kt
+++ app/src/main/java/com/riftlab/app/MainActivity.kt
@@ -7,12 +7,17 @@ import androidx.core.view.WindowCompat
 import com.riftlab.app.overlay.RiftOverlayService
 import com.riftlab.app.stream.StreamLauncher
 import com.riftlab.app.ui.RiftLabRoot
+import com.riftlab.app.ui.RiftTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         WindowCompat.setDecorFitsSystemWindows(window, true)
-        setContent { RiftLabRoot() }
+        setContent {
+            RiftTheme {
+                RiftLabRoot()
+            }
+        }
     }
 
     override fun onStart() {
