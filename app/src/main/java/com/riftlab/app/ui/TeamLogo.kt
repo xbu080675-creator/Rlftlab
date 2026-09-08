9f6d9792bac41bdd5f722291e5f9df42c861f213
PocketSpace <xbu080675@gmail.com>
2026-09-09 02:44:33 +0800

    fix: import dp for team logo shape

--- app/src/main/java/com/riftlab/app/ui/TeamLogo.kt
+++ app/src/main/java/com/riftlab/app/ui/TeamLogo.kt
@@ -10,6 +10,7 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import coil.compose.SubcomposeAsyncImage
 import coil.compose.SubcomposeAsyncImageContent
