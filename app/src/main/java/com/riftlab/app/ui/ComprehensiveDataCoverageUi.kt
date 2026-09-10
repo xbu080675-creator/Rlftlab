af2978278ac79f6a00651e008c4fd21a35f0007f
PocketSpace <xbu080675@gmail.com>
2026-09-10 19:52:05 +0800

    fix: use RowScope weight without internal import

--- app/src/main/java/com/riftlab/app/ui/ComprehensiveDataCoverageUi.kt
+++ app/src/main/java/com/riftlab/app/ui/ComprehensiveDataCoverageUi.kt
@@ -9,7 +9,6 @@ import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.weight
 import androidx.compose.foundation.shape.CutCornerShape
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
